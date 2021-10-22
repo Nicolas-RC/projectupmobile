838b808e9ee47142119240a60b5b01ef878a0bcc
Nicolas-RC <nrosero93@misena.edu.co>
2021-10-21 21:00:35 -0500

    Account en desarollo

--- app/src/main/java/co/edu/sena/projectupmobile/MainActivity.kt
+++ app/src/main/java/co/edu/sena/projectupmobile/MainActivity.kt
@@ -11,14 +11,18 @@ import com.huawei.hms.support.account.AccountAuthManager
 import com.huawei.hms.support.account.request.AccountAuthParams
 import com.huawei.hms.support.account.request.AccountAuthParamsHelper
 import com.huawei.hms.support.account.service.AccountAuthService
+import com.huawei.hms.support.api.entity.auth.Scope
+import com.huawei.hms.support.hwid.HuaweiIdAuthManager
+import com.huawei.hms.support.hwid.request.HuaweiIdAuthParams
+import com.huawei.hms.support.hwid.request.HuaweiIdAuthParamsHelper
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     // Variables
-    companion object {
-        const val REQUEST_CODE = 8888
-        const val TAG = "MainActivityInfo"
-    }
+
+    private val REQUEST_CODE = 8888
+    private val TAG = "MainActivityInfo"
+    val scopes = listOf(Scope("email"))
 
     // Binding
     lateinit var binding: ActivityMainBinding
@@ -33,26 +37,23 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         // Se envía una autorización de request
-        val authParams : AccountAuthParams =
-            AccountAuthParamsHelper(AccountAuthParams.DEFAULT_AUTH_REQUEST_PARAM)
-                .setIdToken()
-                .createParams()
+        val authParams = HuaweiIdAuthParamsHelper(HuaweiIdAuthParams.DEFAULT_AUTH_REQUEST_PARAM)
+            .setIdToken()
+            .setScopeList(scopes)
+            .createParams()
 
         // Se inicializa le objeto account para la autenticación
-        val service : AccountAuthService =
-            AccountAuthManager.getService(this, authParams)
-
-        // Desplegar la autorización
-        startActivityForResult(service.signInIntent, REQUEST_CODE)
+        val service = HuaweiIdAuthManager.getService(this, authParams)
+        startActivityForResult(service.getSignInIntent(), REQUEST_CODE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 8888) {
-            val authAccountTask = AccountAuthManager.parseAuthResultFromIntent(data)
-            if (authAccountTask.isSuccessful) {
+            val authHuaweiIdTask = HuaweiIdAuthManager.parseAuthResultFromIntent(data)
+            if (authHuaweiIdTask.isSuccessful) {
                 // El inicio de sesión es correcto, y el código de autorización ha sido obtenido.
-                val authAccount = authAccountTask.result
+                val authAccount = authHuaweiIdTask.result
                 binding.textView.apply {
                     append(" ${authAccount.displayName}")
                     if (authAccount.email.isNotEmpty()) {
@@ -62,8 +63,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 Log.i(TAG, "Codigo de autorizacion:" + authAccount.authorizationCode + authAccount.idToken)
             } else {
                 // Error al iniciar sesión
-                binding.textView.append("Fallo al iniciar sesion:" + (authAccountTask.exception as ApiException).statusCode)
-                Log.e(TAG, "Fallo al iniciar sesion:" + (authAccountTask.exception as ApiException).statusCode)
+                binding.textView.append("Fallo al iniciar sesion:" + (authHuaweiIdTask.exception as ApiException).statusCode)
+                Log.e(TAG, "Fallo al iniciar sesion:" + (authHuaweiIdTask.exception as ApiException).statusCode)
             }
         }
     }
